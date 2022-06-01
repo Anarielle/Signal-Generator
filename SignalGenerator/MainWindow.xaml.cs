8fda31b552d87bf7d706bd4c2617c70326ecf14c
Anarielle A <anarielle.a@gmail.com>
2022-06-01 19:49:43 +0300

    first commit

---
+++ SignalGenerator/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SignalGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly double[] dataY = new double[500];
+        readonly Stopwatch Stopwatch = Stopwatch.StartNew();
+        readonly ScottPlot.Plottable.VLine VerticalLine;
+        int NextIndex = 0;
+
+        public MainWindow()
+        {
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+            sPhase.Maximum = Math.PI * 2;
+            sPhase.Minimum = 0;
+            pOriginalSignal.Plot.Title("Исходный сигнал");
+            pRecievedSignal.Plot.Title("Полученный сигнал");
+            pHarmonics.Plot.Title("Гармоники");
+            pSpectrum.Plot.Title("Спектр исходного сигнала");
+
+            pOriginalSignal.Plot.SetAxisLimits(0, dataY.Length, -10, 10);
+            pOriginalSignal.Plot.AddSignal(dataY);
+            VerticalLine = pOriginalSignal.Plot.AddVerticalLine(0, width: 2);
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            dispatcherTimer.Tick += timer1_Tick;
+            dispatcherTimer.Start();
+
+            DispatcherTimer dispatcherTimer1 = new DispatcherTimer();
+            dispatcherTimer1.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer1.Tick += timer2_Tick;
+            dispatcherTimer1.Start();
+        }
+
+        public Signal signal = new Signal();
+
+        public void AddDataPoint()
+        {
+            double amplitude = sAmplitude.Value;
+            double phase = sPhase.Value;
+            double frequency = sFrequency.Value;
+            double period = 1 / frequency;
+            double time = frequency * Stopwatch.Elapsed.TotalSeconds + phase;
+
+            if ((bool)rbHarmonic.IsChecked)
+            {
+                this.dataY[NextIndex] = amplitude * Math.Sin(2 * Math.PI * time);
+            }
+            if ((bool)rbSquare.IsChecked)
+            {
+                this.dataY[NextIndex] = amplitude * Math.Sign(Math.Sin(2f * Math.PI * time)); 
+            }
+            if ((bool)rbTriangle.IsChecked)
+            {                
+                this.dataY[NextIndex] = amplitude * (1f - 4f * (float)Math.Abs(Math.Round(time - 0.25f) - (time - 0.25f)));
+            }
+
+            NextIndex += 1;
+            if (NextIndex >= this.dataY.Length)
+                NextIndex = 0;
+
+            VerticalLine.X = NextIndex;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AddDataPoint();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pOriginalSignal.Render();
+        }
+    }
+}
---
+++ SignalGenerator/Signals.cs
@@ -0,0 +1,79 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalGenerator
+{
+    public class Signal
+    {
+        public double Amplitude { get; set; } = 0;
+        public double Phase { get; set; } = 0;
+        public double Frequency { get; set; } = 0;
+        //public double Constant { get; set; } = 0;
+        // double Period = 1 / Frequency;
+
+        //public Signal(double amplitude, double phase, double frequency)
+        //{
+        //    Amplitude = amplitude;
+        //    Phase = phase;
+        //    Frequency = frequency;
+        //}
+
+
+
+        public double[] HarmonicSignal(double t)
+        {
+            double[] yValues = Generate.Sinusoidal(500, 500, Frequency, Amplitude, phase: Phase);
+
+            //double[] yValues = new double[500];
+            //for (int i = 0; i < yValues.Length; i++)
+            //{
+            //    yValues[i] = Amplitude * (float)Math.Sin(2f * Math.PI * (Frequency * i + Phase));
+            //}          
+
+            return yValues;
+        }
+
+        public double[] TriangleSignal(double[] dataX)
+        {
+            //double[] yValues = Generate.Triangle(500, 50, 0, -Amplitude / 2, Amplitude / 2);
+            double[] yValues = new double[500];
+            for (int i = 0; i < yValues.Length; i++)
+            {
+                yValues[i] = Amplitude * (1f - 4f * (float)Math.Abs(Math.Round((Frequency * i + Phase) - 0.25f) - ((Frequency * i + Phase) - 0.25f)));
+            }
+
+
+            //double halfPeriod = 1 / Frequency / 2;
+            //for (int i = 0; i < yValues.Length; i++)
+            //{
+            //    yValues[i] = (Amplitude / halfPeriod) * (halfPeriod - Math.Abs(dataX[i] % (2 * halfPeriod) - halfPeriod));
+            //}
+            return yValues;
+
+            //value = amplitude * (1f - 4f * (float)Math.Abs(Math.Round(t - 0.25f) - (t - 0.25f)));
+        }
+
+        public double[] SquareSignal()
+        {
+            // double lowDuration = 1 / Frequency + Phase;
+            // double highDuration = 1 / Frequency;
+            // double[] yValues = Generate.Square(500, (int)lowDuration, (int)highDuration, -Amplitude / 2, Amplitude / 2);
+
+            double[] yValues = new double[500];
+            double pi = 0;
+            for (int i = 0; i < yValues.Length; i++)
+            {
+                yValues[i] = Amplitude * Math.Sign(Math.Sin(2 * Math.PI * Frequency + Phase)); ;
+                pi += Math.PI;
+            }
+
+            return yValues;
+
+            //value = amplitude * Math.Sign(Math.Sin(2f * Math.PI * t));
+        }
+    }
+}
